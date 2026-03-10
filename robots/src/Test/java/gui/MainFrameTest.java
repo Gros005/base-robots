0fadb4cf57da2ca35fc528f1351da85879e21661
stanislavnetunaev12-arch <stanislavnetunaev12@gmail.com>
2026-03-10 07:49:26 +0500

    2 задача из списка

--- robots/src/Test/java/gui/MainFrameTest.java
+++ robots/src/Test/java/gui/MainFrameTest.java
@@ -48,6 +48,47 @@ class MainFrameTest {
         assertEquals(String.valueOf(expectedWidth), props.getProperty("main.w"), "Ширина не совпала!");
         assertEquals(String.valueOf(expectedHeight), props.getProperty("main.h"), "Высота не совпала!");
     }
+    @Test
+    void testRestoreWindowGeometry() throws IOException {
+        Properties props = new Properties();
+        props.setProperty("main.x", "200");
+        props.setProperty("main.y", "200");
+        props.setProperty("main.w", "1000");
+        props.setProperty("main.h", "900");
+
+        Path configPath = WindowStateManager.getConfigPath();
+        Files.createDirectories(configPath.getParent());
+        try (java.io.OutputStream os = Files.newOutputStream(configPath)) {
+            props.store(os, null);
+        }
+
+        mainFrame = new MainApplicationFrame();
+
+        assertEquals(1000, mainFrame.getWidth(), "Ширина не восстановилась из файла!");
+        assertEquals(900, mainFrame.getHeight(), "Высота не восстановилась из файла!");
+        assertEquals(200, mainFrame.getX(), "Координата X не восстановилась!");
+    }
+
+    @Test
+    void testSaveMaximizedState() {
+        mainFrame = new MainApplicationFrame();
+
+        mainFrame.setExtendedState(javax.swing.JFrame.MAXIMIZED_BOTH);
+
+
+        WindowStateManager.save(mainFrame);
+
+        Properties props = new Properties();
+        try (InputStream is = Files.newInputStream(WindowStateManager.getConfigPath())) {
+            props.load(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals(String.valueOf(javax.swing.JFrame.MAXIMIZED_BOTH),
+                props.getProperty("main.extendedState"),
+                "Состояние 'Развернуто на весь экран' не сохранилось!");
+    }
 
     @AfterEach
     void tearDown() {
